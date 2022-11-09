b27ec8f18953a0bd0096557072d2b468c9a2f4ab
ThaarushiN <thaarushiherath@gmail.com>
2022-11-09 23:51:03 +0530

    DBUTIL ver1

--- StaffManagementSystem/src/main/java/employee/ctrl/EmployeeDBUtil.java
+++ StaffManagementSystem/src/main/java/employee/ctrl/EmployeeDBUtil.java
@@ -6,23 +6,39 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.*;
 
+import com.mysql.jdbc.PreparedStatement;
+
 public class EmployeeDBUtil {
 	public static void validate(String username, String password) {
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			String url="jdbc:sqlserver://localhost:1433;databaseName=StaffManagementSyst";
-			Connection con=DriverManager.getConnection(url, "root", " ");
+			String url="jdbc:sqlserver://DESKTOP-FJEQ3PG/SQLEXPRESS;databaseName=StaffManagementSyst";
+			Connection con=DriverManager.getConnection(url, "webDev", "191083");
+			
 			Statement st=con.createStatement();
 			
 			String sql="SELECT EXISTS(SELECT * FROM Employee WHERE username='"+username+"' AND pssWrd='"+password+"' )";
 			ResultSet rs=st.executeQuery(sql);
 			
-			int result=Integer.parseInt(sql);
+			boolean isValidt=Boolean.parseBoolean(sql);
 			
-			if(result==1) {
-				String sql1=
+			if(isValidt==true) {
+				
+				String sql_q_1="SELECT empNo FROM EMPLOYEE WHERE username='"+username+"' AND pssWrd='"+password+"' )";
+				int empNo=Integer.parseInt(sql_q_1);
+				
+				java.sql.PreparedStatement p=con.prepareStatement("SELECT EXISTS(SELECT * FROM Manager WHERE empNo=?)");
+				
+				p.setInt(1, empNo);
+				
+				
+				Boolean isManager=
+				
 			}
-		}catch(Exception e) {
+			
+			 
+			
+		}catch(Exception e) 
 			
 		}
 	}
